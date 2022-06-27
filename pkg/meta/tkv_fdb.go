ecf06508c6b85c3dab255b8d18f9a7f6f86adc79
fengyu <1424214557@qq.com>
2022-06-28 02:11:37 +0800

    fdb : suport fdb for metadata engine

--- pkg/meta/tkv_fdb.go
+++ pkg/meta/tkv_fdb.go
@@ -17,6 +17,9 @@
 package meta
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/apple/foundationdb/bindings/go/src/fdb"
 )
 
@@ -24,6 +27,8 @@ func init() {
 	Register("fdb", newKVMeta)
 	drivers["fdb"] = newFdbClient
 }
+
+const DEBUG = true
 
 type fdbTxn struct {
 	t fdb.Transaction
@@ -34,8 +39,18 @@ type fdbClient struct {
 }
 
 func newFdbClient(address string) (tkvClient, error) {
-	db, err := fdb.OpenDatabase(address)
-	return withPrefix(&fdbClient{db}, []byte("")), err
+	args := strings.Split(address, ":")
+	err := fdb.APIVersion(610)
+	if err != nil {
+		fmt.Printf("Unable to set API version: %v\n", err)
+		return &tikvClient{}, err
+	}
+	db, err := fdb.OpenDatabase(args[0])
+	prefix := ""
+	if len(args) == 2 {
+		prefix = args[1]
+	}
+	return withPrefix(&fdbClient{db}, append([]byte(prefix), 0xFD)), err
 }
 
 func (c *fdbClient) name() string {
@@ -47,56 +62,145 @@ func (c *fdbClient) txn(f func(kvTxn) error) error {
 	if err != nil {
 		return err
 	}
+	defer func(e *error) {
+		if r := recover(); r != nil {
+			fe, ok := r.(error)
+			if ok {
+				*e = fe
+			} else {
+				panic(r)
+			}
+		}
+	}(&err)
 	if err = f(&fdbTxn{tx}); err != nil {
+		tx.Reset()
 		return err
 	}
 	return tx.Commit().Get()
 }
 
 func (c *fdbClient) scan(prefix []byte, handler func(key, value []byte)) error {
-
+	tx, err := c.client.CreateTransaction()
+	if err != nil {
+		return err
+	}
+	iter := tx.GetRange(
+		fdb.KeyRange{Begin: fdb.Key(prefix), End: fdb.Key(nextKey(prefix))},
+		fdb.RangeOptions{},
+	).Iterator()
+	for iter.Advance() {
+		r := iter.MustGet()
+		handler(r.Key, r.Value)
+	}
+	//fmt.Println("scan ----------------")
+	return nil
 }
 
 func (c *fdbClient) reset(prefix []byte) error {
-
+	tx, err := c.client.CreateTransaction()
+	if err != nil {
+		panic(err)
+	}
+	tx.ClearRange(fdb.KeyRange{
+		Begin: fdb.Key(prefix),
+		End:   fdb.Key(nextKey(prefix)),
+	})
+	//fmt.Println("fdbClient reset !----------------")
+	return tx.Commit().Get()
 }
 
 func (c *fdbClient) close() error {
-
+	c = &fdbClient{}
+	//fmt.Println("fdbClient close !----------------")
+	return nil
 }
 
 func (c *fdbClient) shouldRetry(err error) bool {
-
+	ret := strings.Contains(err.Error(), "Transaction not committed") || strings.Contains(err.Error(), "Operation aborted")
+	/*fmt.Println(err.Error())
+	if ret {
+		fmt.Println("should retry ")
+	}*/
+	return ret
 }
 
 func (tx *fdbTxn) get(key []byte) []byte {
-
+	//fmt.Println("fdbTxn get !----------------")
+	return tx.t.Get(fdb.Key(key)).MustGet()
 }
 
 func (tx *fdbTxn) gets(keys ...[]byte) [][]byte {
-
+	ret := make([][]byte, 0)
+	for _, key := range keys {
+		val := tx.t.Get(fdb.Key(key)).MustGet()
+		ret = append(ret, val)
+	}
+	//fmt.Println("fdbTxn gets !----------------")
+	return ret
+}
+func (tx *fdbTxn) range0(begin, end []byte) fdb.RangeIterator {
+	return *(tx.t.GetRange(
+		fdb.KeyRange{Begin: fdb.Key(begin), End: fdb.Key(end)},
+		fdb.RangeOptions{},
+	).Iterator())
 }
 func (tx *fdbTxn) scanRange(begin, end []byte) map[string][]byte {
-
+	ret := make(map[string][]byte, 0)
+	iter := tx.range0(begin, end)
+	for iter.Advance() {
+		r := iter.MustGet()
+		ret[string(r.Key)] = r.Value
+	}
+	//fmt.Println("fdbTxn scanRange !----------------")
+	return ret
 }
 func (tx *fdbTxn) scanKeys(prefix []byte) [][]byte {
-
+	ret := make([][]byte, 0)
+	iter := tx.range0(prefix, nextKey(prefix))
+	for iter.Advance() {
+		ret = append(ret, iter.MustGet().Key)
+	}
+	//fmt.Println("fdbTxn scanKey !----------------")
+	return ret
 }
 func (tx *fdbTxn) scanValues(prefix []byte, limit int, filter func(k, v []byte) bool) map[string][]byte {
-
+	ret := make(map[string][]byte, 0)
+	iter := tx.range0(prefix, nextKey(prefix))
+	for iter.Advance() {
+		r := iter.MustGet()
+		if filter(r.Key, r.Value) {
+			ret[string(r.Key)] = r.Value
+		}
+	}
+	//fmt.Println("fdbTxn scanValue !----------------")
+	return ret
 }
 func (tx *fdbTxn) exist(prefix []byte) bool {
-
+	iter := tx.range0(prefix, nextKey(prefix))
+	//fmt.Println("fdbTxn exist !----------------")
+	return iter.Advance()
 }
 func (tx *fdbTxn) set(key, value []byte) {
-
+	//fmt.Println("fdbTxn set !----------------")
+	tx.t.Set(fdb.Key(key), fdb.Key(value))
 }
 func (tx *fdbTxn) append(key []byte, value []byte) []byte {
-
+	tx.t.AppendIfFits(fdb.Key(key), fdb.Key(value))
+	//fmt.Println("fdbTxn append !----------------")
+	return tx.t.Get(fdb.Key(key)).MustGet()
 }
 func (tx *fdbTxn) incrBy(key []byte, value int64) int64 {
-
+	new := parseCounter(tx.t.Get(fdb.Key(key)).MustGet())
+	if value != 0 {
+		new += value
+		tx.t.Set(fdb.Key(key), fdb.Key(packCounter(new)))
+	}
+	//fmt.Println("fdbTxn increaby !----------------")
+	return new
 }
 func (tx *fdbTxn) dels(keys ...[]byte) {
-
+	for _, key := range keys {
+		tx.t.Clear(fdb.Key(key))
+	}
+	//fmt.Println("fdbTxn dels !----------------")
 }
